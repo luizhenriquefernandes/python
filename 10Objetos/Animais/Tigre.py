51199ce8343d70f7fc324d433ad2a1a3fe85fca9
luizhenriquefernandes <luizhenrique.bio@gmail.com>
2023-08-04 16:35:47 -0300

    Correcao de classes em python

--- 10Objetos/Animais/Tigre.py
+++ 10Objetos/Animais/Tigre.py
@@ -1,7 +1,14 @@
-class Tigre:
-    def __init__(self,nome):
-        self._nome = nome
-    def get_nome(self):
-        return self._nome
-    def set_nome(self,novoNome):
-        self._nome = novoNome
+from .Animal import Animal
+'''o usar o ponto (.) antes do nome do módulo na importação, 
+você indica que está importando um módulo relativo dentro do 
+mesmo pacote. Dessa forma, o Python entenderá que a classe 
+Tigre está no mesmo pacote Animais e encontrará a definição 
+correta.'''
+class Tigre(Animal):
+    def __init__(self,nome,idade):
+        super().__init__(nome)
+        self._idade = idade
+    def get_idade(self):
+        return self._idade
+    def set_idade(self,novaIdade):
+        self._idade = novaIdade
--- 10Objetos/objeto.py
+++ 10Objetos/objeto.py
@@ -1,10 +1,10 @@
 '''Orientação a objeto como já foi criado a classe Animais
 Agora iremos chamar a classe Animais em Pyhton'''
-from Animais.Tigre import Tigre
 from Animais.Animal import Animal
-tigreObjeto = Tigre("Tigrão")
+from Animais.Tigre import Tigre
+tigreObjeto = Animal("Tigrão")
 nomeT = tigreObjeto.get_nome()
 print(nomeT)
 print(f'''O nome é {tigreObjeto.get_nome()}''')
-leaoObjeto = Animal("Simba", 21)
+leaoObjeto = Tigre("Simba", 41)
 print(f'''O nome é {leaoObjeto.get_nome()} e a idade é {leaoObjeto.get_idade()}''')
