0db4f0c483ecc901055615dca1e29504ad42d989
luizhenriquefernandes <luizhenrique.bio@gmail.com>
2023-08-04 16:37:36 -0300

    Correcao de classes em python

--- 10Objetos/Animais/Tigre.py
+++ 10Objetos/Animais/Tigre.py
@@ -2,7 +2,7 @@ from .Animal import Animal
 '''o usar o ponto (.) antes do nome do módulo na importação, 
 você indica que está importando um módulo relativo dentro do 
 mesmo pacote. Dessa forma, o Python entenderá que a classe 
-Tigre está no mesmo pacote Animais e encontrará a definição 
+Animal que é a classe principal está no mesmo pacote Animais e encontrará a definição 
 correta.'''
 class Tigre(Animal):
     def __init__(self,nome,idade):
